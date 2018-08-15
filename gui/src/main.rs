c4a4ac126afec2274a05b97ae6235fea21feac97
pearisgreen <grproessl@web.de>
2018-08-15 21:41:03 +0200

    update

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,47 +1,11 @@
-#[macro_use]
 extern crate yew;
+extern crate counter;
+
 use yew::prelude::*;
-
-type Context = ();
-
-struct Model { }
-
-enum Msg {
-    DoIt,
-}
-
-impl Component<Context> for Model {
-    // Some details omitted. Explore the examples to get more.
-
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, _: &mut Env<Context, Self>) -> Self {
-        Model { }
-    }
-
-    fn update(&mut self, msg: Self::Message, _: &mut Env<Context, Self>) -> ShouldRender {
-        match msg {
-            Msg::DoIt => {
-                // Update your model on events
-                true
-            }
-        }
-    }
-}
-
-impl Renderable<Context, Model> for Model {
-    fn view(&self) -> Html<Context, Self> {
-        html! {
-            // Render your model here
-            <button onclick=|_| Msg::DoIt,>{ "Click me!" }</button>
-        }
-    }
-}
+use counter::Model;
 
 fn main() {
     yew::initialize();
-    let app: App<_, Model> = App::new(());
-    app.mount_to_body();
+    App::<Model>::new().mount_to_body();
     yew::run_loop();
 }
